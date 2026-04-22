d4cf5c6669c906c68eeddaad3bbace7ca128debb
baihu <baihu3210@gmail.com>
2026-04-22 11:10:55 +0800

    Refactor RollerConfig to use RollerConstant for motor neutral mode

--- src/main/java/frc/robot/subsystems/Intake/Roller/RollerConfig.java
+++ src/main/java/frc/robot/subsystems/Intake/Roller/RollerConfig.java
@@ -1,13 +1,12 @@
 package frc.robot.subsystems.Intake.Roller;
 
 import com.GFL.lib.hardware.config.MotorConfig;
-import com.GFL.lib.hardware.config.MotorConfig.NeutralMode;
 
 public class RollerConfig {
     public static MotorConfig getRollerMotorConfig() {
         MotorConfig config = new MotorConfig();
 
-        config.setNeutralMode(NeutralMode.COAST);
+        config.setNeutralMode(RollerConstant.rollerMotorNeutralMode);
         
         return config;
     }
--- src/main/java/frc/robot/subsystems/Intake/Roller/RollerConstant.java
+++ src/main/java/frc/robot/subsystems/Intake/Roller/RollerConstant.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems.Intake.Roller;
 
 import com.GFL.lib.Factory.MotorFactory.MotorModel;
+import com.GFL.lib.hardware.config.MotorConfig.NeutralMode;
 
 public class RollerConstant {
     public static final MotorModel rollerMotorModel = MotorModel.Krakenx60;
+
+    public static final NeutralMode rollerMotorNeutralMode = NeutralMode.COAST;
     public static final int rollerMotorID = 32;
 
     public static final double intakeSpeed = 0.5;
