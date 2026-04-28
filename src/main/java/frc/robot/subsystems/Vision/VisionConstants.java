a4b992a880058c0ebfe43f5420d9f120120d44f4
baihu <baihu3210@gmail.com>
2026-04-28 19:26:01 +0800

    Remove unused imports from VisionConstants.java

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.Vision;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Rotation3d;
