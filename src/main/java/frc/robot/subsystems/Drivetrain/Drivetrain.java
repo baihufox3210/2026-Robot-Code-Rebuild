80b176e7ed288174bbae95867f9a915887c364cf
baihu <baihu3210@gmail.com>
2026-04-29 22:00:26 +0800

    Remove unused PoseEstimator import from Drivetrain class

--- src/main/java/frc/robot/subsystems/Drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/Drivetrain.java
@@ -8,7 +8,6 @@ import com.pathplanner.lib.config.RobotConfig;
 
 import dev.doglog.DogLog;
 import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
