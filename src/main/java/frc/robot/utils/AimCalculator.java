7acffd299499bcb724b66d05cdf25cd4e1f2a487
baihu <baihu3210@gmail.com>
2026-04-29 18:13:50 +0800

    Add AimCalculator and FieldTransformer classes for target heading and pose transformation

---
+++ src/main/java/frc/robot/utils/AimCalculator.java
@@ -0,0 +1,9 @@
+package frc.robot.utils;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class AimCalculator {
+    public static Rotation2d getTargetHeading() {
+        
+    }
+}
---
+++ src/main/java/frc/robot/utils/FieldTransformer.java
@@ -0,0 +1,10 @@
+package frc.robot.utils;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import frc.robot.RobotConstants;
+
+public class FieldTransformer {
+    public static Translation3d mirror(Translation3d pose) {
+        return new Translation3d(RobotConstants.fieldSize.getX() - pose.getX(), pose.getY(), pose.getZ());
+    }
+}
