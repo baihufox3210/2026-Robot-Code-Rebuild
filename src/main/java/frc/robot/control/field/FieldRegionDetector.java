97ffce55c0543bdeb8dd455128f5741c95a58539
baihu <baihu3210@gmail.com>
2026-04-29 21:48:39 +0800

    Refactor FieldRegionDetector to simplify getCurrentRegion method and remove poseSupplier dependency

--- src/main/java/frc/robot/control/field/FieldRegionDetector.java
+++ src/main/java/frc/robot/control/field/FieldRegionDetector.java
@@ -2,21 +2,11 @@ package frc.robot.control.field;
 
 import static edu.wpi.first.units.Units.Meters;
 
-import java.util.function.Supplier;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.RobotConstants;
 
-public class FieldRegionDetector {
-    private final Supplier<Pose2d> poseSupplier;
-
-    public FieldRegionDetector(Supplier<Pose2d> poseSupplier) {
-        this.poseSupplier = poseSupplier;
-    }
-    
-    public FieldRegion getCurrentRegion() {
-        Pose2d currentPose = poseSupplier.get();
-
+public class FieldRegionDetector {   
+    public FieldRegion getCurrentRegion(Pose2d currentPose) {
         double x = currentPose.getX();
 
         double redStartLine = RobotConstants.fieldSize.getX() - RobotConstants.allianceDepth.in(Meters);
@@ -26,7 +16,7 @@ public class FieldRegionDetector {
         if(x > redStartLine) return FieldRegion.RedAlliance;
         return FieldRegion.Neutral;
     }
-    
+
     public enum FieldRegion {
         RedAlliance, Neutral, BlueAlliance;
     }
