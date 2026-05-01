7a182c8a3f53eae7a6b9d43b3167e548438d6e78
baihu <baihu3210@gmail.com>
2026-05-01 11:02:46 +0800

    Remove unused import for CommandXboxController in Drive command

--- src/main/java/frc/robot/Commands/Drivetrain/Drive.java
+++ src/main/java/frc/robot/Commands/Drivetrain/Drive.java
@@ -2,7 +2,6 @@ package frc.robot.Commands.Drivetrain;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Drivetrain.Drivetrain;
 import frc.robot.utils.InputTransform;
 
