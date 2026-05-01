64d87af2fbbed6936cfdfeec2be1b421849acf39
baihu <baihu3210@gmail.com>
2026-05-01 10:17:59 +0800

    Refactor AimSystem to use a Supplier for currentPose and update calculate method accordingly

--- src/main/java/frc/robot/control/aiming/AimSystem.java
+++ src/main/java/frc/robot/control/aiming/AimSystem.java
@@ -1,12 +1,21 @@
 package frc.robot.control.aiming;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import frc.robot.control.geometry.FieldGeometry;
 import frc.robot.control.targeting.TargetSelector;
 
 public class AimSystem {
-    public static ShotParameters calculate(Pose2d currentPose) {
+    private Supplier<Pose2d> poseSupplier;
+
+    public AimSystem(Supplier<Pose2d> poseSupplier) {
+        this.poseSupplier = poseSupplier;
+    }
+
+    public ShotParameters calculate() {
+        Pose2d currentPose = poseSupplier.get();
         Translation3d targetPose = TargetSelector.getTargetPose(currentPose);
 
         return new ShotParameters(
