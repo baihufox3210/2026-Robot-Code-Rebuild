854a717198aaaa8131815397d9a65b0f2058298a
baihu <baihu3210@gmail.com>
2026-05-25 18:53:56 +0800

    Refactor RobotContainer to replace CommandJoystick with CommandXboxController and update button bindings for improved control

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Commands.Climber.Climbing;
 import frc.robot.Commands.Climber.ToggleClimberPosition;
 import frc.robot.Commands.Drivetrain.Drive;
@@ -21,7 +21,7 @@ import frc.robot.subsystems.Drivetrain.Drivetrain;
 import frc.robot.subsystems.Vision.Vision;
 
 public class RobotContainer {
-	private final CommandJoystick controller = new CommandJoystick(0);
+	private final CommandXboxController controller = new CommandXboxController(0);
 	private final Drivetrain drivetrain = Drivetrain.getInstance();
 
 	private final AimSystem aimSystem;
@@ -33,6 +33,9 @@ public class RobotContainer {
 
 		aimSystem = new AimSystem(drivetrain::getPose);
 
+		SmartDashboard.putNumber("targetPose x", aimSystem.calculate().targetPose().getX());
+		SmartDashboard.putNumber("targetPose y", aimSystem.calculate().targetPose().getY());
+
 		drivetrain.setDefaultCommand(new Drive(controller));
 		drivetrain.configurePathPlanner();
 
@@ -42,17 +45,17 @@ public class RobotContainer {
   	}
 
   	private void configureBindings() {
-		controller.button(5).onTrue(new TogglePivotPosition());
+		controller.leftBumper().onTrue(new TogglePivotPosition());
 		
-		controller.button(2).onTrue(new ToggleClimberPosition());
-		controller.button(4).onTrue(new Climbing());
+		controller.x().onTrue(new ToggleClimberPosition());
+		controller.b().onTrue(new Climbing());
 
-		controller.button(3).toggleOnTrue(new Intaking());
+		controller.a().toggleOnTrue(new Intaking());
 
-		controller.button(6).whileTrue(
+		controller.rightBumper().whileTrue(
 			Commands.parallel(
 				new DriveToAim(controller, aimSystem::calculate),
-				new Shoot(aimSystem::calculate).onlyIf(drivetrain::isAtHeading)
+				new Shoot(aimSystem::calculate)
 			)
 		);
 	}
