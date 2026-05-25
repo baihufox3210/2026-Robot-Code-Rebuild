89785ec8303d77d6dbc3819171f8c58c1d4ac28b
baihu <baihu3210@gmail.com>
2026-05-25 18:54:26 +0800

    Add targetPose to ShotParameters for improved aiming calculations

--- src/main/java/frc/robot/control/aiming/AimSystem.java
+++ src/main/java/frc/robot/control/aiming/AimSystem.java
@@ -20,7 +20,8 @@ public class AimSystem {
 
         return new ShotParameters(
             FieldGeometry.toTargetHeading(currentPose, targetPose),
-            ShotCalculator.getOptimalSetpoint(currentPose, targetPose)
+            ShotCalculator.getOptimalSetpoint(currentPose, targetPose),
+            targetPose
         );
     }
 }
