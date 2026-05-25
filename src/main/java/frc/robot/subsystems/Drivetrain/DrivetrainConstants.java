d52c9502a94859d06ab2513a44fc8da7bc680ea2
baihu <baihu3210@gmail.com>
2026-05-25 19:19:46 +0800

    Remove unused imports for Pose2d and Rotation2d in DrivetrainConstants

--- src/main/java/frc/robot/subsystems/Drivetrain/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/DrivetrainConstants.java
@@ -10,8 +10,6 @@ import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 
