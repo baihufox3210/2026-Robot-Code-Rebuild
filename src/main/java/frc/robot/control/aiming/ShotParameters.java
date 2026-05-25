a534a1eab79db85f248b0a11e11453cfb1138530
baihu <baihu3210@gmail.com>
2026-05-25 18:54:18 +0800

    Add targetPose to ShotParameters for enhanced aiming data

--- src/main/java/frc/robot/control/aiming/ShotParameters.java
+++ src/main/java/frc/robot/control/aiming/ShotParameters.java
@@ -1,8 +1,10 @@
 package frc.robot.control.aiming;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 
 public record ShotParameters(
     Rotation2d targetHeading,
-    double targetVelocity
+    double targetVelocity,
+    Translation3d targetPose
 ) {}
