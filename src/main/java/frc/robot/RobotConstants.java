39b2e16e4dad34db1ca3fd097f7ae3f6580af7b1
baihu <baihu3210@gmail.com>
2026-05-26 12:59:43 +0800

    Refactor initialPose definition in RobotConstants for clarity and consistency

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.Meters;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.units.measure.Distance;
@@ -12,13 +14,15 @@ public class RobotConstants {
     public static final double deadband = 0.1;
     public static final double deltaSecond = 0.02;
 
+    public static final Pose2d initialPose = new Pose2d(8, 4, Rotation2d.kZero);
+
     public static final Translation2d fieldSize = new Translation2d(16.54, 8.07);
 
     public static final Translation3d allianceHub = new Translation3d(4.07, 4.03, 1.9);
     
     public static final Translation3d rightShotPoint = new Translation3d(2.0, 2.0, 0);
     public static final Translation3d leftShotPoint = new Translation3d(
-        fieldSize.getX() - rightShotPoint.getX(), rightShotPoint.getY(), rightShotPoint.getZ()
+        rightShotPoint.getX(), fieldSize.getY() - rightShotPoint.getY(), rightShotPoint.getZ()
     );
 
     public static final Distance allianceDepth = Meters.of(4.03);
